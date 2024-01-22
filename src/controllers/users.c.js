6d42791e91c1535dc5f9dac1ccc5516dbdf917f1
21120470 <nhathuynguyen217@gmail.com>
2024-01-22 13:21:33 +0700

    update user infor

--- src/controllers/users.c.js
+++ src/controllers/users.c.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.m');
 const customError = require('../utils/custom-error');
 
+
 module.exports = {
     userProfileController: async (req, res, next) => {
         res.render('customer/profile', { loginUser: req.user });
@@ -17,9 +18,10 @@ module.exports = {
     updateProfileController: async(req, res, next) => {
         console.log(req.body);
         try {
-            await userModel.update(req.body, req.params.id);
+            await userModel.update(req.body, req.params.customerId);
             res.redirect('/customer/profile');
         } catch (error) {
+            console.log(error);
             next(new customError(error.message, 503));
         }
     }
--- src/routers/users.r.js
+++ src/routers/users.r.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.c');
+const multer = require('multer');
+const path = require('path');
+
+//create storagte for avatar
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadPath = path.join(__dirname, '../', 'public/images/users/avatars');
+        cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+        const arr = file.originalname.split('.');
+        const ext = arr[arr.length - 1];
+        const fieldName = req.user?.id + '.' + ext;
+        req.body.avatar = fieldName;
+        console.log(fieldName);
+        cb(null, fieldName);
+    }
+})
+
+const upload = multer({ storage: storage })
+
 
 router.get('/customer/profile', usersController.userProfileController);
-router.post('/customer/profile/:id', usersController.updateProfileController);
+router.post('/customer/profile/:customerId', upload.single('avatar'), usersController.updateProfileController);
 
 module.exports = router;
