5f92fbd35eb193d10b364b7a926b75806179db57
21120477 <vuonghuynhkhai4869@gmail.com>
2024-01-22 10:29:48 +0700

    giao dien product list, create, update product

--- src/controllers/products.c.js
+++ src/controllers/products.c.js
@@ -1,4 +1,5 @@
 const bookModel = require('../models/book.m');
+const genreModel = require('../models/genre.m');
 const customError = require('../utils/custom-error');
 
 module.exports = {
@@ -31,7 +32,13 @@ module.exports = {
 
     productsController: async (req, res, next) => {
         try {
-            res.render('admin/products/products', { loginUser: req.user, products: true });
+            const productList = await bookModel.getAll();
+            for(let book of productList) {
+                const genre = await genreModel.get(book.genre);
+                book.genre = genre.name;
+            }
+
+            res.render('admin/products/products', { loginUser: req.user, products: true, productList: productList });
         } catch (error) {
             next(new customError(error.message, 503));
         }
@@ -39,7 +46,12 @@ module.exports = {
 
     createProductController: async (req, res, next) => {
         try {
-            res.render('admin/products/create-product', { loginUser: req.user, products: true });
+            const genreList = await genreModel.getAll();
+            res.render('admin/products/create-product', { 
+                loginUser: req.user, 
+                products: true,
+                genreList: genreList
+            });
         } catch (error) {
             next(new customError(error.message, 503));
         }
@@ -47,7 +59,25 @@ module.exports = {
 
     updateProductController: async (req, res, next) => {
         try {
-            res.render('admin/products/update-product', { loginUser: req.user, products: true });
+            const bookId = req.params.productId;
+            const book = await bookModel.get(bookId);
+
+            if(!book) {
+                throw new customError('Can not find this book!', 404);
+            }
+
+            const genre = await genreModel.get(book.genre);
+            book.genre = genre.name;
+            const genreList = await genreModel.getAll();
+
+            //render update
+            res.render('admin/products/update-product', { 
+                loginUser: req.user,
+                products: true,
+                book: book,
+                genreList: genreList
+            });
+
         } catch (error) {
             next(new customError(error.message, 503));
         }
--- src/models/genre.m.js
+++ src/models/genre.m.js
@@ -2,7 +2,7 @@ const db = require('../utils/db');
 const tableName = 'genres';
 const tableId = 'id';
 
-module.exports = class User {
+module.exports = class Genre {
     constructor(obj) {
         this.id = obj?.id;
         this.name = obj?.name;
@@ -10,12 +10,21 @@ module.exports = class User {
 
     static async get(id) {
         const response = await db.getOneOrNone(tableName, [{ fieldName: tableId, value: id }]);
-        const user = new User(response);
-        return user;
+        if(response) {
+            const genre = new Genre(response);
+            return genre;
+        }
+        return null;
+    }
+
+    static async getAll() {
+        const response = await db.getAll(tableName);
+        const genreList = response.map((genre) => new Genre(genre));
+        return genreList;
     }
 
     static async add(genre) {
-        const response = await db.insert(tableName, user, tableId);
+        const response = await db.insert(tableName, genre, tableId);
         return response;
     }
 };
