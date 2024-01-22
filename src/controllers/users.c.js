6d55d38df41adbbddf56e4120b5731140fd81c0b
21120470 <nhathuynguyen217@gmail.com>
2024-01-22 18:53:49 +0700

    update default avatar

--- src/controllers/users.c.js
+++ src/controllers/users.c.js
@@ -4,7 +4,7 @@ const customError = require('../utils/custom-error');
 
 module.exports = {
     userProfileController: async (req, res, next) => {
-        res.render('customer/profile', { loginUser: req.user });
+        res.render('customer/profile', { loginUser: req.user } );
     },
 
     // JUST FOR ADMIN
@@ -16,7 +16,6 @@ module.exports = {
         }
     },
     updateProfileController: async(req, res, next) => {
-        console.log(req.body);
         try {
             await userModel.update(req.body, req.params.customerId);
             res.redirect('/customer/profile');
