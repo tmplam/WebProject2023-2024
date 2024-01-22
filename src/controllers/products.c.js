a02e3fb253006bedb3262861cd22e9afe44d53c1
21120477 <vuonghuynhkhai4869@gmail.com>
2024-01-22 12:23:57 +0700

    add product, update product

--- src/controllers/products.c.js
+++ src/controllers/products.c.js
@@ -1,6 +1,8 @@
 const bookModel = require('../models/book.m');
 const genreModel = require('../models/genre.m');
 const customError = require('../utils/custom-error');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     homeController: async (req, res, next) => {
@@ -57,6 +59,48 @@ module.exports = {
         }
     },
 
+    createProductControllerPost: async (req, res, next) => {
+        try {
+            //validate
+            //if invalid then delete req.files and throw error
+
+            const book = {
+                author: req.body.author,
+                isbn: req.body.isbn,
+                title: req.body.title,
+                genre: req.body.genre,
+                price: req.body.price,
+                stock_quantity: req.body.quantity,
+                description: req.body.description,
+            }
+
+            let i = 1;
+            for(let file of req.files) {
+                const ext = file.mimetype.substring(file.mimetype.indexOf('/')+1);
+                const oldPath = file.path;
+                if(i == 1) {
+                    const newPath = path.join(file.destination, `front-cover/${file.filename}.${ext}`);
+                    fs.renameSync(oldPath, newPath);
+                    book.front_cover_image = `/images/products/front-cover/${file.filename}.${ext}`;
+                }
+                else {
+                    const newPath = path.join(file.destination, `back-cover/${file.filename}.${ext}`);
+                    fs.renameSync(oldPath, newPath);
+                    book.back_cover_image = `/images/products/back-cover/${file.filename}.${ext}`;
+                }
+                i = i + 1;
+            }
+
+            //add to database
+            await bookModel.add(book);
+
+            res.redirect('/admin/products');
+        } catch (error) {
+            next(new customError(error.message, 503));
+        }
+    }
+    ,
+
     updateProductController: async (req, res, next) => {
         try {
             const bookId = req.params.productId;
@@ -82,4 +126,61 @@ module.exports = {
             next(new customError(error.message, 503));
         }
     },
+
+    updateProductControllerPost: async (req, res, next) => {
+        try {
+            const bookId = req.params.productId;
+            const book = await bookModel.get(bookId);
+
+            if(!book) {
+                throw new customError('Can not find this book!', 404);
+            }
+
+            book.author = req.body.author;
+            book.isbn = req.body.isbn;
+            book.title = req.body.title;
+            book.genre = req.body.genre;
+            book.description = req.body.description;
+            book.price = Number(req.body.price);
+            book.stock_quantity = Number(req.body.quantity);
+
+            //update cover img
+            let i = 1;
+            for(let file of req.files) {
+                const ext = file.mimetype.substring(file.mimetype.indexOf('/')+1);
+                const oldPath = file.path;
+                if(i == 1) {
+                    const newPath = path.join(file.destination, `front-cover/${file.filename}.${ext}`);
+                    fs.renameSync(oldPath, newPath);
+                    //delete old image
+                    fs.unlinkSync(path.join(__dirname,'../',`/public${book.front_cover_image}`));
+                    book.front_cover_image = `/images/products/front-cover/${file.filename}.${ext}`;
+                }
+                else {
+                    const newPath = path.join(file.destination, `back-cover/${file.filename}.${ext}`);
+                    fs.renameSync(oldPath, newPath);
+                    //delete old image
+                    fs.unlinkSync(path.join(__dirname,'../',`/public${book.back_cover_image}`));
+                    book.back_cover_image = `/images/products/back-cover/${file.filename}.${ext}`;
+                }
+                i = i + 1;
+            }
+            await bookModel.update(book, bookId);
+
+            const genre = await genreModel.get(book.genre);
+            book.genre = genre.name;
+            const genreList = await genreModel.getAll();
+            //render update
+            res.render('admin/products/update-product', { 
+                loginUser: req.user,
+                products: true,
+                book: book,
+                genreList: genreList
+            });
+
+        } catch (error) {
+            next(new customError(error.message, 503));
+        }
+    }
+
 };
--- src/routers/admin.r.js
+++ src/routers/admin.r.js
@@ -4,11 +4,20 @@ const productsController = require('../controllers/products.c');
 const genresController = require('../controllers/genres.c');
 const ordersController = require('../controllers/orders.c');
 const usersController = require('../controllers/users.c');
+//multer
+const path = require('path');
+const multer = require('multer')
+const upload = multer({ dest: path.join(__dirname, '../public/images/products') })
+
+
+
 
 router.get('/dashboard', productsController.dashBoardController);
 router.get('/products', productsController.productsController);
-router.get('/products/create', productsController.createProductController);
+router.get('/products/create',productsController.createProductController);
+router.post('/products/create', upload.array('cover-img', 2),productsController.createProductControllerPost);
 router.get('/products/:productId/edit', productsController.updateProductController);
+router.post('/products/:productId/edit', upload.array('cover-img', 2) ,productsController.updateProductControllerPost);
 
 router.get('/genres', genresController.genresController);
 
